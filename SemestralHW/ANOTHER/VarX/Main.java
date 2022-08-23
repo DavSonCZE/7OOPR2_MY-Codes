4a2bcfd3a9519414986d7b764b3592d70237ce9b
DavSonCZE <40635601+DavSonCZE@users.noreply.github.com>
2022-08-23 09:02:09 +0200

    Create Main.java

---
+++ SemestralHW/ANOTHER/VarX/Main.java
@@ -0,0 +1,17 @@
+package osu.david;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final int cons = 1900; //kvůli rokům
+
+        String[] emails = { "david@seznam.cz", "abraham@s.cz", "zofie@a.cz"};
+        Person p1 = new Person("David", new Date(1998-cons, Calendar.JANUARY,14), Arrays.asList(emails));
+
+        System.out.println("Name: " + p1.getName() + ", Date: "+p1.getDateOfBirth() + ", Emails: ");
+        System.out.print(p1.getEmails());
+    }
+}
